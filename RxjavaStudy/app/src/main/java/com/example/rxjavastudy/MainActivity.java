1f533751e3ee3cfe7b1cd260c87cc24dc46e1a3b
mina <mina0774@hanmail.net>
2020-01-28 11:55:12 +0900

    first commit

---
+++ RxjavaStudy/app/src/main/java/com/example/rxjavastudy/MainActivity.java
@@ -0,0 +1,15 @@
+package com.example.rxjavastudy;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    //첫번째
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
